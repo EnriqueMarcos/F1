b698616c4f785d2b8b563279012538eb8093bb00
hectormv11 <h.montoya.v02@gmail.com>
2023-11-21 12:57:27 +0100

    Para ti

--- src/Proyecto/Combinacion.java
+++ src/Proyecto/Combinacion.java
@@ -1,5 +1,34 @@
 package Proyecto;
 
 public class Combinacion {
-
+	
+	int numPartida;
+	int[] numeros;
+	int reintegro;
+	
+	
+	
+	public Combinacion(int partida, int[] combi, int extra) {
+		
+		numPartida = partida;
+		
+		numeros = new int[6];
+		combi = numeros;
+		
+		reintegro = extra;
+		
+		
+	}
+	
+	public int numeroDeAciertos(Combinacion combiPremiada) {
+		
+		
+		return 0;
+		
+		
+		
+	}
+		
+	
+	
 }
