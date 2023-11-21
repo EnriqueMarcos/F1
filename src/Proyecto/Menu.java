9a90d35f4544d448405e7309d145342034fa3c6e
IvanPuyi <ivan.casdom.1@educa.jcyl.es>
2023-11-21 13:38:52 +0100

    Iniciado el menú y añadida la clase Historial.

--- src/Proyecto/Menu.java
+++ src/Proyecto/Menu.java
@@ -1,5 +1,26 @@
 package Proyecto;
 
+import java.util.Scanner;
+
 public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("Opciones:" + "\n" + "1. Jugar" + "\n" + "2. Ver historial" + "\n" + "3. Salir");
+		Scanner scannerMenu = new Scanner (System.in);
+		int numeroMenu = scannerMenu.nextInt();
+		if (numeroMenu == 1) {
+			System.out.println("---");
+			Primitiva menuPrimitiva = new Primitiva();
+		}
+		else if (numeroMenu == 2) {
+			System.out.println("2222");
+			Historial menuHistorial = new Historial();
+		}
+		else if (numeroMenu == 3) {
+			scannerMenu.close();
+		}else {
+			System.out.println("Introduzca un número entre 1 y 3.");
+		}
+	}
 
 }
