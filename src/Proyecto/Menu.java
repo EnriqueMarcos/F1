9c2157bb9146da907028422ef1f83e8fd88fc8dc
IvanPuyi <ivan.casdom.1@educa.jcyl.es>
2023-11-21 14:13:27 +0100

    Actualizado menú.

--- src/Proyecto/Menu.java
+++ src/Proyecto/Menu.java
@@ -4,25 +4,23 @@ import java.util.Scanner;
 
 public class Menu {
 
-
 	public static void main(String[] args) {
 		System.out.println("Opciones:" + "\n" + "1. Jugar" + "\n" + "2. Ver historial" + "\n" + "3. Salir");
 		Scanner scannerMenu = new Scanner (System.in);
 		int numeroMenu = scannerMenu.nextInt();
+		while (numeroMenu != 1 && numeroMenu != 2 && numeroMenu != 3) {
+			System.out.println("Introduzca un número entre 1 y 3.");
+			numeroMenu = scannerMenu.nextInt();
+		}
 		if (numeroMenu == 1) {
-			System.out.println("---");
 			Primitiva menuPrimitiva = new Primitiva();
 		}
 		else if (numeroMenu == 2) {
-			System.out.println("2222");
 			Historial menuHistorial = new Historial();
 		}
 		else if (numeroMenu == 3) {
-			scannerMenu.close();
-		}else {
-			System.out.println("Introduzca un número entre 1 y 3.");
+			System.out.println("¡Hasta pronto!");
 		}
 	}
-	
 
 }
