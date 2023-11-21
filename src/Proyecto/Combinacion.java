15b1733f9336b1737139e47798dcad4ffc75dbcf
hectormv11 <h.montoya.v02@gmail.com>
2023-11-21 13:30:33 +0100

    Clase combinacion creada

--- src/Proyecto/Combinacion.java
+++ src/Proyecto/Combinacion.java
@@ -8,7 +8,8 @@ public class Combinacion {
 	int[] numeros;
 	int reintegro;
 	
-	
+	int min = 1;
+	int max = 20;
 	
 	public Combinacion(int partida, int[] combi, int extra) {
 		
@@ -19,13 +20,39 @@ public class Combinacion {
 		
 		reintegro = extra;
 		
+	}
+	
+	public Combinacion generarCombinacion() {
 		
+		int partida = 0;
+		int[] array = new int[6];
+		int numExtra = 0;
+		
+		Combinacion combi = new Combinacion(partida, array, numExtra);
+		
+		for (int i = 0; i < numeros.length; i++ ) {
+			
+			array[i] = numeroAleatorio(min, max);
+			
+		}
+		
+		partida += 1;
+		numExtra = numeroAleatorio(min, max);
+		
+		return combi;
 	}
 	
 	public int numeroDeAciertos(Combinacion combiPremiada) {
+		int aciertos = 0;
 		
-		//.
-		return 0;
+		for (int i = 0; i < numeros.length; i++ ) {
+			for (int j = 0; j < numeros.length; j++ )
+			if (numeros[i] == combiPremiada.numeros[j]) {
+				aciertos++;
+			}
+			
+		}
+		return aciertos;
 		
 		
 		
@@ -33,7 +60,7 @@ public class Combinacion {
 		
 	public static int numeroAleatorio(int min, int max){
 		Random rn = new Random();
-		return rn.nextInt(max - min + 1) + min;
+		return rn.nextInt(min - max);
 	}
 	
 }
