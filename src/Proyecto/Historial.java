11ef4247937bb7e243fcb099edf60450123116ca
Enrique Marcos <enriquem.sanred@educa.jcyl.es>
2023-11-24 10:23:25 +0100

    Esto es lo que se me ha ocurrido usar un array para guardar 10 objetos y un contador

--- src/Proyecto/Historial.java
+++ src/Proyecto/Historial.java
@@ -1,10 +1,24 @@
 package Proyecto;
 
 public class Historial {
+
+	public int contadorPartidas = 0;
+	public Combinacion[] partidas;
 	
-	public void main(String[] args) {
-		Primitiva historialPrimitiva = new Primitiva();
-		System.out.println(historialPrimitiva);
-	}
-	
+	 public Historial() {
+	        this.partidas = new Combinacion[10];
+	        this.contadorPartidas = 0;
+	    }
+	 
+	 public void guardarPartidas(Combinacion partida) {
+		 if (contadorPartidas < 10) {
+			 partidas[contadorPartidas] = partida;
+			 contadorPartidas++;
+		 }else {
+			contadorPartidas = 0;
+			 partidas[contadorPartidas] = partida;
+			 contadorPartidas++;
+		 }
+	 }
 }
+
